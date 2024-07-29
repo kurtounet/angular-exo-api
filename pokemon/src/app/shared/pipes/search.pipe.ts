2ef5e22b522a34b977e77597306b2dc69d247546
Anthony bossut <bossut.anthony@gmail.com>
2024-07-29 16:53:40 +0200

    update

---
+++ pokemon/src/app/shared/pipes/search.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IPokemon } from '../interfaces/pokemon.model';
+
+@Pipe({
+  name: 'search',
+  standalone: true
+})
+export class SearchPipe implements PipeTransform {
+
+   
+   transform(values: IPokemon[], terms: string):IPokemon[] {     
+    
+    if(values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) ).length > 1){
+      return values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) );
+    }else{
+     return values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) );;
+    }
+      
+    
+    
+    //return values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) );  
+   
+  }
+}
