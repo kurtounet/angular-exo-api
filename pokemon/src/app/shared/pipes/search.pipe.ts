b069442020b44eb59609392bfce2a01fed9cf61e
Anthony bossut <bossut.anthony@gmail.com>
2024-07-29 18:48:37 +0200

    update

--- pokemon/src/app/shared/pipes/search.pipe.ts
+++ pokemon/src/app/shared/pipes/search.pipe.ts
@@ -10,15 +10,13 @@ export class SearchPipe implements PipeTransform {
    
    transform(values: IPokemon[], terms: string):IPokemon[] {     
     
-    if(values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) ).length > 1){
-      return values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) );
-    }else{
-     return values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) );;
-    }
-      
-    
-    
-    //return values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) );  
+    // if(values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) ).length > 1){
+    //   return values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) );
+    // }else{
+    //  return values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) );;
+    // }
+          
+   return values.filter(value=> value.name.fr?.toLowerCase().startsWith(terms.toLowerCase()) );  
    
   }
 }
